140245cd83797c22eb7ba3f8dc0a51df90e2f5c1
DevLomoSE <jonathancggs@gmail.com>
2021-08-25 16:32:33 -0500

    FEAT: clase contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // *Nombre de la etiqueta para este componente
   templateUrl: './app.component.html',
+  // template: 'app-root', // *<= renderiza todo el string de dicho componente
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'bases';
+  public contador: number = 0;
+  public contador2: number = 0;
+  public base: number = 5;
+
+  actionContador(valor: number, accion: string ): number{
+    this.contador = (accion === 'sumar')
+                    ? this.contador += valor
+                    : this.contador -= valor;
+    return this.contador;
+  }
+
+  acumular(accion: string): void{
+    this.contador2 = (accion === 'sumar')
+                    ? this.contador2 += this.base
+                    : this.contador2 -= this.base;
+  }
+
+  sumar = () => { this.contador += 1; };
+  restar = () => { this.contador -= 1; };
 }
